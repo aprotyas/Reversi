05749771193c398d4a2b044e9664646083c99dac
Abrar Rahman Protyasha <abrar.rahman98@gmail.com>
2022-12-21 05:04:43 -0500

    Introduce TUI/GUI canvas types
    Signed-off-by: Abrar Rahman Protyasha <abrar.rahman98@gmail.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,6 @@
 git_root=$(git rev-parse --show-toplevel)
 pushd ${git_root}
-cmake -G Ninja -S . -B build -DBUILD_TESTING=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+# Hard coding the TUI build for now
+cmake -G Ninja -S . -B build -DBUILD_TESTING=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DBUILD_APP_VERSION=TUI
 cmake --build build
 popd
--- src/othello_app.cpp
+++ src/othello_app.cpp
@@ -1,8 +1,42 @@
+#include <cstddef>
 #include <iostream>
+#include <type_traits>
 
 #include "othello/othello.hpp"
 
+struct GUI {};
+struct TUI {};
+
+template <typename CanvasType,
+          typename = std::enable_if_t<std::is_same_v<CanvasType, TUI> ||
+                                      std::is_same_v<CanvasType, GUI>>>
+struct CanvasImpl {
+  static void show_intro();
+  static void show_rules();
+};
+
+template <>
+struct CanvasImpl<TUI> {
+  static void show_intro() {
+    std::cout << " ------------------------------------------------------------"
+                 "------ \n";
+    std::cout << " |                      WELCOME TO OTHELLO                   "
+                 "     | \n";
+    std::cout << " ------------------------------------------------------------"
+                 "------ \n";
+  }
+
+  static void show_rules() {}
+};
+
 int main() {
+#ifdef GUI_CANVAS
+  using Canvas = CanvasImpl<GUI>;
+#elif defined TUI_CANVAS
+  using Canvas = CanvasImpl<TUI>;
+#endif
+  Canvas::show_intro();
+
   Othello othello;
 
   // othello.init(player_names);
